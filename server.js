4a1fdbebcbd69f98ae760c368afa7f5976b7f783
Aleksander Konnerup <Alek.k@hotmail.com>
2018-11-01 12:10:13 +0100

    - mybe fixed heroku deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/src')));
+
 app.use('/api', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
@@ -21,13 +21,14 @@ app.use('/api', function(req, res, next) {
 
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+app.use(express.static(__dirname + 'dist/ittweb-assignment2'))
 
 app.use('/api/', api)
 app.all('*', function(req, res) {
-  res.status(200).sendFile(__dirname + '/src/index.html');
+  res.status(200).sendFile(path.join(__dirname, 'dist/ittweb-assignment2','index.html'));
 });
 const port = (process.env.PORT || 8080);
-
+ 
 app.listen(port,function(){
   console.log("Server running on: " + port);
 });
